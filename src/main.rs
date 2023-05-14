6adc4f432afa20880a1c18df750a17890136eb9a
Barbara Bach <133556283+barbarabach@users.noreply.github.com>
2023-05-14 01:32:04 -0400

    control flow and loop
    Co-developed-by: Caroline Munro <133555034+carolinemunro@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    let mut number = 9;
+    while number != 0 {
+        if number % 2 == 0 {
+            number = number / 3;
+        } else {
+            number = number * 6;
+        }
+        print!("--> {}", number);
+    }
+    println!();
+    println!("The end!");
 }
 
